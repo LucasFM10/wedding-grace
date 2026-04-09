973abfcc3463ca9e7fcdbeecc3c4d415e0e18cb1
Lucas Farias de Medeiros <lucas.medeiros@tjpb.jus.br>
2026-04-09 12:05:05 -0300

    Ajustes no título

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,12 @@ const lato = Lato({
 });
 
 export const metadata: Metadata = {
-  title: "Lucas & Amanda se casam",
-  description: "Celebração do Sacramento do Matrimônio de Lucas e Amanda. Ofereça um terço e contribua com nossa caminhada.",
+  title: "Lucas & Amanda se casam - Rezem por nós'",
+  description: "Celebração do sacramento do Matrimônio de Lucas e Amanda. Ofereça um terço e contribua com nossa caminhada.",
   authors: [{ name: "Lucas & Amanda" }],
   openGraph: {
-    title: "Lucas & Amanda — Casamento Católico",
-    description: "Celebração do Sacramento do Matrimônio. Ofereça um terço e participe do nosso ElevadorParaDois.",
+    title: "Lucas & Amanda se casam - Rezem por nós'",
+    description: "Celebração do sacramento do Matrimônio de Lucas e Amanda. Ofereça um terço e contribua com nossa caminhada.",
     type: "website",
   },
   twitter: {
